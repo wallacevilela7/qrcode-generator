2efc938099305f652ee00c1fc04f314d85bffaab
wallacevilela7 <wallace.vilela9@gmail.com>
2025-05-19 15:10:56 -0300

    Create spring app

---
+++ src/main/java/tech/wvs/qrcodegenerator/QrcodeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.qrcodegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrcodeGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/wvs/qrcodegenerator/QrcodeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.qrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
